07354140c6d5694dff5d18c30683509d16db0d79
marsel1323 <amn132313@gmail.com>
2019-02-23 13:55:09 +0300

    add koa/cors

--- server.js
+++ server.js
@@ -4,11 +4,13 @@ const mount = require('koa-mount');
 const graphqlHTTP = require('koa-graphql');
 const schema = require('./graphql/schema');
 const initDB = require('./database');
-
+const cors = require('@koa/cors');
 
 const {PORT} = config;
 
 const app = new Koa();
+
+app.use(cors())
 
 app.use(mount('/graphql', graphqlHTTP({
 	schema: schema,
